8d8967c4cafab4243fe80b84b02da83e0ecff923
Miracle <2893496202@qq.com>
2023-10-02 17:11:12 +0800

    实现用户删除、查找服务

--- src/main/java/com/miracle/usercenter/controller/UserController.java
+++ src/main/java/com/miracle/usercenter/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.miracle.usercenter.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.miracle.usercenter.common.BaseResponse;
 import com.miracle.usercenter.common.ErrorCode;
 import com.miracle.usercenter.common.ResultUtils;
@@ -9,13 +10,15 @@ import com.miracle.usercenter.model.domain.request.UserLoginRequest;
 import com.miracle.usercenter.model.domain.request.UserRegisterRequest;
 import com.miracle.usercenter.service.UserService;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.miracle.usercenter.contant.UserConstant.ADMIN_ROLE;
+import static com.miracle.usercenter.contant.UserConstant.USER_LOGIN_STATE;
 
 /**
  * 用户 接口
@@ -85,6 +88,58 @@ public class UserController {
         }
         int result = userService.userLogout(request);
         return ResultUtils.success(result);
+    }
+
+    /**
+     * 管理员搜索用户
+     * @param username
+     * @param request
+     * @return
+     */
+    @GetMapping("/search")
+    public BaseResponse<List<User>> searchUsers(String username, HttpServletRequest request) {
+        if (!isAdmin(request)) {
+            throw new BusinessException(ErrorCode.NO_AUTH, "缺少管理员权限");
+        }
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        if (StringUtils.isNotBlank(username)) {
+            queryWrapper.like("username", username);
+        }
+        List<User> userList = userService.list(queryWrapper);
+        List<User> list = userList.stream().map(user -> userService.getSafetyUser(user)).collect(Collectors.toList());
+        return ResultUtils.success(list);
+    }
+
+    /**
+     * 管理员删除用户
+     * @param id id
+     * @param request
+     * @return
+     */
+    @PostMapping("/delete")
+    public BaseResponse<Boolean> deleteUser(@RequestBody long id, HttpServletRequest request) {
+        if (!isAdmin(request)) {
+            throw new BusinessException(ErrorCode.NO_AUTH);
+        }
+        if (id <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        boolean b = userService.removeById(id);
+        return ResultUtils.success(b);
+    }
+
+
+    /**
+     * 是否为管理员
+     *
+     * @param request
+     * @return
+     */
+    private boolean isAdmin(HttpServletRequest request) {
+        // 仅管理员可查询
+        Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
+        User user = (User) userObj;
+        return user != null && user.getUserRole() == ADMIN_ROLE;
     }
 
 }
